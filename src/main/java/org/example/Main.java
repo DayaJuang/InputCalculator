e9c6b3849df6acda6524d0206242753ae7ab75b4
daya <dayajuangmahaputra@gmail.com>
2022-11-10 20:27:15 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,40 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage(){
+        // Create a scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Create a variable of sum and avg
+        int sum = 0;
+        int count = 0;
+
+        while(true){
+            // for checking if the input valid or not
+            boolean hasNextInt = scanner.hasNextInt();
+            if(hasNextInt){
+                int num = scanner.nextInt();
+                sum += num;
+                count++;
+            }else{
+                if(count == 0){
+                    count++;
+                }
+                break;
+            }
+        }
+
+        double pureAvg = (double) sum / count;
+        long avg = Math.round(pureAvg);
+
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+
+        scanner.close();
     }
 }
